d4a3232706597d76532434ac5be976899b55b535
Junella Caringal <junellacaringal@gmail.com>
2022-05-03 13:45:18 -0700

    Registered models to admin file. Views from previous commit message that didn't push. Added for loop for todolist details in todo lists html, but this is not working. Will continue to work on linking detail page later today.

--- brain_two/todos/views.py
+++ brain_two/todos/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView
+from todos.models import TodoItem, TodoList
+from django.urls import reverse_lazy
 
 # Create your views here.
+class TodoListView(ListView):
+    model = TodoList
+    template_name = "todos/todo_lists.html"
+
+
+class TodoListCreateView(CreateView):
+    model = TodoList
+    template_name = "todos/todolist_create.html"
+    fields = ["name"]
+
+    def form_invalid(self, form):
+        context = super().form_invalid(form)
+
+    success_url = reverse_lazy("todos/todo_lists.html")
+
+
+class TodoListDetailView(DetailView):
+    model = TodoList
+    template_name = "todos/todolist_detail.html"
+
+
+class TodoItemCreateView(CreateView):
+    model = TodoItem
+    template_name = "todos/todoitem_create.html"
+    fields = ["task", "due_date", "is_completed", "lists"]
+
+    def form_invalid(self, form):
+        context = super().form_invalid(form)
+
+    success_url = reverse_lazy("todos/todo_lists.html")
