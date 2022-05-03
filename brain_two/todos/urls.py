6cca3b3d6cd12a71b2c6e18e725fc946f3aa2668
Junella Caringal <junellacaringal@gmail.com>
2022-05-03 16:32:55 -0700

    Added delete path for todolist and update paths for todolist and todoitem. Created associated views. Added code to todolist detail template. Started blank template files for todoitem and todolist update and todolist delete.

--- brain_two/todos/urls.py
+++ brain_two/todos/urls.py
@@ -4,6 +4,9 @@ from todos.views import (
     TodoListCreateView,
     TodoListDetailView,
     TodoItemCreateView,
+    TodoListDeleteView,
+    TodoListUpdateView,
+    TodoItemUpdateView,
 )
 
 urlpatterns = [
@@ -11,4 +14,15 @@ urlpatterns = [
     path("<int:pk>/", TodoListDetailView.as_view(), name="todolist_detail"),
     path("create/", TodoListCreateView.as_view(), name="todolist_create"),
     path("items/create/", TodoItemCreateView.as_view(), name="todoitem_create"),
+    path(
+        "<int:pk>/delete/", TodoListDeleteView.as_view(), name="todolist_delete"
+    ),
+    path(
+        "<int:pk>/edit/", TodoListUpdateView.as_view(), name="todolist_update"
+    ),
+    path(
+        "items/<int:pk>/edit/",
+        TodoItemUpdateView.as_view(),
+        name="todoitem_update",
+    ),
 ]
--- brain_two/todos/views.py
+++ brain_two/todos/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    DeleteView,
+    UpdateView,
+)
 from todos.models import TodoItem, TodoList
 from django.urls import reverse_lazy
 
@@ -25,10 +31,40 @@ class TodoListDetailView(DetailView):
     template_name = "todos/todolist_detail.html"
 
 
+class TodoListDeleteView(DeleteView):
+    model = TodoList
+    template_name = "todos/todolist_delete.html"
+
+    def form_invalid(self, form):
+        context = super().form_invalid(form)
+
+    success_url = reverse_lazy("todos/todo_lists.html")
+
+
+class TodoListUpdateView(UpdateView):
+    model = TodoList
+    template_name = "todos/todolist_update.html"
+
+    def form_invalid(self, form):
+        context = super().form_invalid(form)
+
+    success_url = reverse_lazy("todos/todo_lists.html")
+
+
 class TodoItemCreateView(CreateView):
     model = TodoItem
     template_name = "todos/todoitem_create.html"
     fields = ["task", "due_date", "is_completed", "list"]
+
+    def form_invalid(self, form):
+        context = super().form_invalid(form)
+
+    success_url = reverse_lazy("todos/todo_lists.html")
+
+
+class TodoItemUpdateView(UpdateView):
+    model = TodoItem
+    template_name = "todos/todoitem_update.html"
 
     def form_invalid(self, form):
         context = super().form_invalid(form)
