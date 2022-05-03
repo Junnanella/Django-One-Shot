90a5905a2e020ba99f8e1cc3bddf4fff5abc37a5
Junella Caringal <junellacaringal@gmail.com>
2022-05-03 13:12:16 -0700

    Added urls for todolist list,detail,create views and item create view. Created associated views. Started base html file and todolist list html file. Working

---
+++ brain_two/todos/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from todos.views import (
+    TodoListView,
+    TodoListCreateView,
+    TodoListDetailView,
+    TodoItemCreateView,
+)
+
+urlpatterns = [
+    path("", TodoListView.as_view(), name="todolists"),
+    path("<int:pk>/", TodoListDetailView.as_view(), name="todolist_detail"),
+    path("create/", TodoListCreateView.as_view(), name="todolist_create"),
+    path("items/create", TodoItemCreateView.as_view(), name="todoitem_create"),
+]
